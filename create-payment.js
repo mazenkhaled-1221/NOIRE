1165aab5653d8ff1975f845f831920098132b540
Mazen Khaled <mazenkhalid122009@gmail.com>
2026-08-18 03:27:57 +0300

    NOIRE

--- create-payment.js
+++ create-payment.js
@@ -263,7 +263,8 @@ export default async function handler(req, res) {
 return res.status(200).json({
     success: true,
     orderNumber,
-    checkoutUrl
+    clientSecret: data.client_secret
+
 });
 
 
--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(
 ===================================================== */
 
 app.post(
-    "/api/create-payment",
+    "/create-payment",
     async (req, res) => {
 
         try {
